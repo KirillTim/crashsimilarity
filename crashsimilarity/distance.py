85c403f3050c265a53138d3d67c62bcd0c9e6ea1
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2017-05-11 16:43:31 +0300

    dist

---
+++ crashsimilarity/distance.py
@@ -0,0 +1,91 @@
+import numpy as np
+from gensim import corpora
+
+from pyemd import emd
+import pyximport
+from sklearn.metrics import pairwise
+
+pyximport.install()
+
+
+class DistanceCalculator(object):
+    def __init__(self, model, w2pos, dist):
+        self.model = model
+        self.w2pos = w2pos
+        self.dist = dist
+
+    def fast_rwmd(self, doc1, doc2):
+        doc1 = [w for w in doc1 if w in self.model]
+        pos1 = [self.w2pos[w] for w in doc1 if w in self.w2pos]
+        doc2 = [w for w in doc2 if w in self.model]
+        pos2 = [self.w2pos[w] for w in doc2 if w in self.w2pos]
+        if pos1 and pos2:
+            s_dists = np.zeros((len(pos2), len(pos1)), dtype=np.double)
+            for i, d in enumerate(pos2):
+                for j, f in enumerate(pos1):
+                    s_dists[i, j] = self.dist[d][f]
+            rwmd = max(np.sum(np.min(s_dists, axis=0)), np.sum(np.min(s_dists, axis=1)))
+        else:
+            rwmd = float('inf')
+        return rwmd
+
+    def _create_distance_matrix(self, dictionary):
+        distances = np.zeros((len(dictionary), len(dictionary)), dtype=np.double)
+        view = list(dictionary.items())
+        for i, w1 in dictionary.items():
+            for j, w2 in view:
+                distances[i, j] = self.dist[int(w1), int(w2)]
+        return distances
+
+    def fast_wmd(self, words1, words2):
+        words1 = [w for w in words1 if w in self.model]
+        words1 = [str(self.w2pos[w]) for w in words1 if w in self.w2pos]
+        words2 = [w for w in words2 if w in self.model]
+        words2 = [str(self.w2pos[w]) for w in words2 if w in self.w2pos]
+
+        dictionary = corpora.Dictionary(documents=[words1, words2])
+        vocab_len = len(dictionary)
+
+        if len(words1) == 0 or len(words1) == 0:
+            return np.double('inf')
+        if vocab_len == 1:
+            return 0.0
+
+        # create bag of words from document
+        def create_bow(doc):
+            norm_bow = np.zeros(vocab_len, dtype=np.double)
+            bow = dictionary.doc2bow(doc)
+            for idx, count in bow:
+                norm_bow[idx] = count / float(len(doc))
+            return norm_bow
+
+        bow1 = create_bow(words1)
+        bow2 = create_bow(words2)
+
+        distances = self._create_distance_matrix(dictionary)
+
+        return emd(bow1, bow2, distances)
+
+    @staticmethod
+    def words_distance(corpus, model):
+        model.init_sims(replace=True)
+        words = set()
+        for trace in corpus:
+            for w in trace.words:
+                words.add(w)
+        words = [w for w in words if w in model]
+        print('len(words) = {}'.format(len(words)))
+        w2pos = dict([(w, i) for i, w in enumerate(words)])
+        wi = [model.wv.vocab[word].index for word in words]
+        wv = np.array([model.wv.syn0norm[i] for i in wi])
+        # dist = 1 - cosine_similarity(wv)
+        # dist = np.zeros((len(wi), len(wi)), dtype=np.double)
+        # for i, t1 in enumerate(wi):
+        #    if i % 500 == 0:
+        #        print (i)
+        #    for j, t2 in enumerate(wi):
+        #        if j > i:
+        #            dist[i, j] = np.sqrt(np.sum((model.wv.syn0norm[t1] - model.wv.syn0norm[t2])**2))
+        #            dist[j, i] = dist[i, j]
+        dist = pairwise.euclidean_distances(wv)
+        return dist, w2pos
--- crashsimilarity/tmp.py
+++ crashsimilarity/tmp.py
@@ -1,13 +1,9 @@
 import json
 
 import logging
-import numpy as np
-import time
 
 from gensim.models import doc2vec
 
-from gensim.corpora.dictionary import Dictionary
-from pyemd import emd
 from datetime import timedelta
 from crashsimilarity.downloader import SocorroDownloader
 from crashsimilarity import utils
@@ -28,78 +24,6 @@ def download_save_day(i):
     day = utils.utc_today() - timedelta(i)
     gen = SocorroDownloader().download_day_crashes(day)
     utils.write_json(utils.crashes_dump_file_path(day, 'Firefox', save_to_dir), gen)
-
-
-# model = Doc2Vec.load('../data/model/dm_d200_all.model')
-# corpus = list(corpus_generator('../data/new_clean_compressed.json'))
-
-
-# Code modified from https://github.com/RaRe-Technologies/gensim/blob/4f0e2ae/gensim/models/keyedvectors.py#L339
-def wmdistance(model, words1, words2, all_distances):
-    dictionary = Dictionary(documents=[words1, words2])
-    vocab_len = len(dictionary)
-
-    # Sets for faster look-up.
-    docset1 = set(words1)
-    docset2 = set(words2)
-
-    distances = np.zeros((vocab_len, vocab_len), dtype=np.double)
-
-    for i, t1 in dictionary.items():
-        for j, t2 in dictionary.items():
-            if t1 not in docset1 or t2 not in docset2:
-                continue
-
-            distances[i, j] = all_distances[model.wv.vocab[t2].index, i]
-
-    if np.sum(distances) == 0.0:
-        # `emd` gets stuck if the distance matrix contains only zeros.
-        logging.info('The distance matrix is all zeros. Aborting (returning inf).')
-        return float('inf')
-
-    # create bag of words from document
-    def create_bow(doc):
-        norm_bow = np.zeros(vocab_len, dtype=np.double)
-        bow = dictionary.doc2bow(doc)
-
-        for idx, count in bow:
-            norm_bow[idx] = count / float(len(doc))
-
-        return norm_bow
-
-    bow1 = create_bow(words1)
-    bow2 = create_bow(words2)
-
-    return emd(bow1, bow2, distances)
-
-
-def rwmd_distances(model, corpus, idx):
-    model.init_sims(replace=True)
-
-    words = corpus[idx].words
-    words = [w for w in words if w in model]
-    logging.info('words in model: {}'.format(len(words)))
-    # Cos-similarity
-    all_distances = np.array(1.0 -
-                             np.dot(model.wv.syn0norm,
-                                    model.wv.syn0norm[[model.wv.vocab[word].index for word in words]].transpose()),
-                             dtype=np.double)
-
-    # Relaxed Word Mover's Distance for selecting
-    t = time.time()
-    distances = []
-    for doc_id in range(0, len(corpus)):
-        doc_words = [model.wv.vocab[word].index for word in corpus[doc_id].words if word in model]
-        if len(doc_words) != 0:
-            word_dists = all_distances[doc_words]
-            rwmd = max(np.sum(np.min(word_dists, axis=0)), np.sum(np.min(word_dists, axis=1)))
-        else:
-            rwmd = float('inf')
-        distances.append((doc_id, rwmd))
-
-    # distances.sort(key=lambda v: v[1])
-    logging.info('First part done in ' + str(time.time() - t) + ' s.')
-    return distances
 
 
 def lcs(a, b):
