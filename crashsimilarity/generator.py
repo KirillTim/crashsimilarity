88fb25a6fb6d21d0521bc365b0d78a1c1fa488a5
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2017-05-29 01:41:43 +0300

    ...

---
+++ crashsimilarity/generator.py
@@ -0,0 +1,30 @@
+import random
+
+
+class Generator(object):
+    def __init__(self, model, vocab, pos2vocab):
+        self._model = model
+        self._vocab = vocab
+        self._pos2vocab = pos2vocab
+
+    def change(self, trace, idx):
+        other = trace[:]
+        for i in idx:
+            t = trace[i]
+            if t in self._vocab:
+                similar = self._model.most_similar(str(self._vocab[t]))
+                for w, _ in similar:
+                    x = self._pos2vocab[int(w)]
+                    if not x.endswith('________'):
+                        other[i] = x
+                        break
+        return other
+
+    def generate(self, trace, count=10, percent=0.33):
+        idx = []
+        new = int(len(trace) * percent)
+        for i in range(count):
+            cur = list(set([random.randint(0, len(trace) - 1) for _ in range(new)]))
+            idx.append(cur)
+        rv = [self.change(trace, i) for i in idx]
+        return rv
--- crashsimilarity/other_distances.py
+++ crashsimilarity/other_distances.py
@@ -1,11 +1,61 @@
+from collections import defaultdict
+
 import numpy as np
+import time
+
+from gensim.models import doc2vec
+from sklearn.cluster import DBSCAN
 
 
-def edit_distance_matrix(corpus, calculator, prog=10):
+def compressed_tagged_corpus(corpus, vocab):
+    compressed = [[str(vocab.get(i, i)) for i in c] for c in corpus]
+    compressed = [doc2vec.TaggedDocument(trace, [i]) for i, trace in enumerate(compressed)]
+    return compressed
+
+
+def dist_for_cluster(cluster, dist):
+    rv = np.zeros((len(cluster), len(cluster)), dtype=np.double)
+    for i, c in enumerate(cluster):
+        for j, v in enumerate(cluster):
+            rv[i, j] = dist[c, v]
+    return rv
+
+
+def dbscan(dist, eps=0.5, min_samples=5):
+    db = DBSCAN(eps=eps, min_samples=min_samples, metric='precomputed').fit(dist)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    return labels, n_clusters, core_samples_mask
+
+
+def labels_to_clusters(labels):
+    clusters = defaultdict(list)
+    for i, c in enumerate(labels):
+        if c != -1:
+            clusters[c].append(i)
+    return clusters
+
+
+def struct_word_dist(w1, w2):
+    parts1 = w1.split('::')
+    parts2 = w2.split('::')
+    if len(parts1) < len(parts2):
+        return struct_word_dist(w2, w1)
+    prefix = 0
+    i = 0
+    while i < len(parts2) and i < len(parts1) and parts1[i] == parts2[i]:
+        prefix += 1
+        i += 1
+    return 1 - float(prefix) / max(len(parts1), len(parts2))
+
+
+def distance_matrix(corpus, calculator, prog=10):
     dist = np.zeros((len(corpus), len(corpus)), dtype=np.double)
     idx = []
     for i in range(len(corpus)):
-        for j in range(i+1, luuen(corpus)):
+        for j in range(i + 1, len(corpus)):
             idx.append((i, j))
     say = len(idx) // prog
     t = time.time()
@@ -16,6 +66,29 @@ def edit_distance_matrix(corpus, calculator, prog=10):
         doc2 = corpus[j].words
         dist[i, j] = dist[j, i] = calculator(doc1, doc2)
     return dist
+
+
+def edit_distance2(s1, s2, ins_cost=lambda a, b: 1, del_cost=lambda a, b: 1, subst_cost=lambda a, b: 1):
+    if len(s1) < len(s2):
+        return edit_distance2(s2, s1, ins_cost, del_cost, subst_cost)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + ins_cost(s1[i], s2[
+                j])  # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + del_cost(s2[j], s2[j - 1])  # than s2
+            substitutions = previous_row[j] + (c1 != c2) * subst_cost(c1, c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
 
 def edit_distance(s1, s2, dist):
     if len(s1) < len(s2):
@@ -29,14 +102,15 @@ def edit_distance(s1, s2, dist):
     for i, c1 in enumerate(s1):
         current_row = [i + 1]
         for j, c2 in enumerate(s2):
-            d = [dist(s1[i], s2[j]), #insert
-                 dist(s2[j], s2[j-1]), #del
-                 dist(c1, c2)] #subst
+            d = [dist(s1[i], s2[j]),  # insert
+                 dist(s2[j], s2[j - 1]),  # del
+                 dist(c1, c2)]  # subst
             d = [2 if i < 0 else i for i in d]
-            insertions = previous_row[j + 1] + d[0] # j+1 instead of j since previous_row and current_row are one character longer
-            deletions = current_row[j] + d[1]      # than s2
+            insertions = previous_row[j + 1] + d[
+                0]  # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + d[1]  # than s2
             substitutions = previous_row[j] + (c1 != c2) * d[2]
             current_row.append(min(insertions, deletions, substitutions))
         previous_row = current_row
-    
+
     return previous_row[-1]
